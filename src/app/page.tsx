e346cd2d503cb3647a4843cb32f6f211db70315f
Ferdinand <bokallitechnology@gmail.com>
2024-03-30 00:29:50 +0100

    Add mobile font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
     return (
         <main className="flex flex-col gap-4 justify-center items-center min-h-screen">
-            <h1 className="text-2xl">Welcome To BOULOT-SUR!</h1>
+            <h1 className="text-base md:text-2xl">Welcome To BOULOT-SUR!</h1>
         </main>
     );
 }
